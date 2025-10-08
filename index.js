5e45acabe6fc026927fb28c58b9f979495d1f962
rronaldo007 <rukundoronaldo4@gmail.com>
2025-10-08 11:55:06 +0200

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("i am learing CI/CD pipeline");
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Starting tests...");
+
+setTimeout(() => {
+    console.log("waiting 3 seconds .....");
+}, 3000);
+
+console.log("All tests completed");
